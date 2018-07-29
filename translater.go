85c61628f61e25ce7d5f941b97f3aedac6caafa7
Armyoftanks <romina.skayanyc@gmail.coms>
2018-07-29 14:14:39 -0400

    <3

--- translater.go
+++ translater.go
@@ -1,61 +1,64 @@
 package main
 
 import (
-	"net/url"
-	"os"
-	"strings"
 	"net/http"
+	"net/url"
+	"strings"
 	"encoding/json"
+	"os"
 	"fmt"
-	"google.golang.org/api/translate/v2"
-	"golang.org/x/oauth2/google"
+	"log"
+	"context"
+	"golang.org/x/text/language"
+	"cloud.google.com/go/translate"
+
 )
 
-/*
+func main() {
 
-OBJECTIVE: TEXT YOUR FRIENDS IN A DIFF LANGUAGE
+	//GOOGLE TRANSLATE API STUFF HERE
 
-1. SEND MESSAGE TO GOOGLE TRANSLATE
-2. RECEIVE TRANSLATION AND STORE INTO MESSAGE
-3. SEND MESSAGE TO FRIENDS
+	ctx := context.Background()
 
-*/
+	gClient, errs := translate.NewClient(ctx)
+	if errs != nil {
+		log.Fatalf("Failed to create client: %v", errs)
+	}
 
-type q string
+	text := os.Args[1]
+	target, errs := language.Parse("ro")
+	if errs != nil {
+		log.Fatalf("Failed to parse target language: %v", errs)
+	}
 
-type target string
+	// Translates the text into Romanian.
+	translations, errs := gClient.Translate(ctx, []string{text}, target, nil)
+	if errs != nil {
+		log.Fatalf("Failed to translate text: %v", errs)
+	}
 
-type source string
+	fmt.Printf("Text: %v\n", text)
+	fmt.Printf("Translation: %v\n", translations[0].Text)
 
-type TranslateTextResponseTranslation struct {
-	TranslateTextResponseTranslation []translations
-}
+	//TWILIO API STUFF HERE
 
-type data struct {
-	TranslateTextResponseTranslation TranslateTextResponseTranslation
-	q q
-	target target
-	source source
-}
+	accountSid := "x"
+	authToken := "x"
 
-
-func main () {
-
-	// GOOGLE TRANSLATE
-
-	urlStr := "https://translation.googleapis.com/language/translate/v2"
+	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
 
 	msgData := url.Values{}
-	msgData.Set("target", os.Args[1])
-	msgData.Set("q", os.Args[2])
-	msgData.Set("model", "nmt")
-	msgData.Set("source", "en")
+	msgData.Set("To", os.Args[2])
+	msgData.Set("From","x")
+	msgData.Set("Body", translations[0].Text)
 	msgDataReader := *strings.NewReader(msgData.Encode())
 
 	client := &http.Client{}
 
 	req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+	req.SetBasicAuth(accountSid, authToken)
 	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	resp, _ := client.Do(req)
 	if (resp.StatusCode >= 200 && resp.StatusCode < 300) {
@@ -63,45 +66,9 @@ func main () {
 		decoder := json.NewDecoder(resp.Body)
 		err := decoder.Decode(&data)
 		if (err == nil) {
-			fmt.Println(data)
+			fmt.Println(data["sid"])
 		}
 	} else {
 		fmt.Println(resp.Status);
 	}
-
-	/* TWILIO
-
-	accountSid := "xxxx"
-	authToken := "xxxx"
-
-	urlStr2 := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
-
-	msgData2 := url.Values{}
-	msgData2.Set("To", os.Args[1])
-	msgData2.Set("From","xxxx")
-	msgData2.Set("Body", os.Args[2])  // <---  I WANT TO REPLACE THIS WITH THE TRANSLATED TEXT FROM GOOGLE TRANSLATE RESPONSE ABOVE
-	msgDataReader2 := *strings.NewReader(msgData2.Encode())
-
-	client2 := &http.Client{}
-
-	req2, _ := http.NewRequest("POST", urlStr2, &msgDataReader2)
-	req2.SetBasicAuth(accountSid, authToken)
-	req2.Header.Add("Accept", "application/json")
-	req2.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	resp2, _ := client2.Do(req2)
-	if (resp2.StatusCode >= 200 && resp2.StatusCode < 300) {
-		var data2 map[string]interface{}
-		decoder2 := json.NewDecoder(resp2.Body)
-		err2 := decoder2.Decode(&data2)
-		if (err2 == nil) {
-			fmt.Println(data2["sid"])
-		}
-	} else {
-		fmt.Println(resp2.Status);
-	}
-
-
-*/
-
 }
---
+++ transv2.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"net/url"
+	"os"
+	"strings"
+	"net/http"
+	"encoding/json"
+	"fmt"
+)
+
+/*
+OBJECTIVE: TEXT YOUR FRIENDS IN A DIFF LANGUAGE
+1. SEND MESSAGE TO GOOGLE TRANSLATE
+2. RECEIVE TRANSLATION AND STORE INTO MESSAGE
+3. SEND MESSAGE TO FRIENDS
+*/
+
+
+
+func main () {
+
+	// GOOGLE TRANSLATE
+
+	urlStr := "https://translation.googleapis.com/language/translate/v2"
+
+	msgData := url.Values{}
+	msgData.Set("target", os.Args[1])
+	msgData.Set("q", os.Args[2])
+	msgData.Set("model", "nmt")
+	msgData.Set("source", "en")
+	msgData.Set("key", "f4c5c971cc1b3caef19232522981649207d438de")
+	msgDataReader := *strings.NewReader(msgData.Encode())
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+	req.Header.Add("Accept", "application/json")
+
+	resp, _ := client.Do(req)
+	if (resp.StatusCode >= 200 && resp.StatusCode < 300) {
+		var data map[string]interface{}
+		decoder := json.NewDecoder(resp.Body)
+		err := decoder.Decode(&data)
+		if (err == nil) {
+			fmt.Println(data)
+		}
+	} else {
+		fmt.Println(resp);
+		fmt.Println(resp.Body)
+	}
+
+	/* TWILIO
+	accountSid := "xxxx"
+	authToken := "xxxx"
+	urlStr2 := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+	msgData2 := url.Values{}
+	msgData2.Set("To", os.Args[1])
+	msgData2.Set("From","xxxx")
+	msgData2.Set("Body", os.Args[2])  // <---  I WANT TO REPLACE THIS WITH THE TRANSLATED TEXT FROM GOOGLE TRANSLATE RESPONSE ABOVE
+	msgDataReader2 := *strings.NewReader(msgData2.Encode())
+	client2 := &http.Client{}
+	req2, _ := http.NewRequest("POST", urlStr2, &msgDataReader2)
+	req2.SetBasicAuth(accountSid, authToken)
+	req2.Header.Add("Accept", "application/json")
+	req2.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp2, _ := client2.Do(req2)
+	if (resp2.StatusCode >= 200 && resp2.StatusCode < 300) {
+		var data2 map[string]interface{}
+		decoder2 := json.NewDecoder(resp2.Body)
+		err2 := decoder2.Decode(&data2)
+		if (err2 == nil) {
+			fmt.Println(data2["sid"])
+		}
+	} else {
+		fmt.Println(resp2.Status);
+	}
+*/
+
+}
